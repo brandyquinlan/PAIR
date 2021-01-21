39a2d820b8fcc6d5b4e5cb7c0d1661df7414891b
Delta9k8b <keaton.brewster@gmail.com>
2021-01-20 21:51:33 -0500

    working on js to dynamically create results page

--- assets/js/results.js
+++ assets/js/results.js
@@ -25,14 +25,14 @@ $(document).ready(function () {
       .then(function (response) {
         console.log(response);
         
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < response.results.length; i++) {
           var col = $('<div>').attr('class', 'col s6'),
             card = $('<div>').attr('class', 'card'),
             cardImageDiv = $('<div>').attr('class', 'card-image waves-effect waves-block waves-light'),
             cardImage = $('<img>').attr({ 'src': response.results[i].image, 'alt': 'image of food' }),
     
             cardContent = $('<div>').attr('class', 'card-content'),
-            contentSpan = $('<span>').attr('class', 'card-title activator grey-text text-darken-4').text("Recipie name"),
+            contentSpan = $('<span>').attr('class', 'card-title activator grey-text text-darken-4 truncate').text(response.results[i].title),
             spanI = $('<i>').attr('class', 'material-icons right'),
             contentJlink = $('<p>').attr('id', 'jumplink').text("Jump to Atricle"),
             jLinkHREF = $('<a>').attr('href', '#'),
@@ -43,7 +43,7 @@ $(document).ready(function () {
             recipeNameH = $('<h6>').attr('id', 'recipeName'),
             hr = $('<hr>'),
             ul = $('<ul>'),
-            prepTime = $('<li>').attr('id', 'prepTimeReveal').text('Prep time: ' + 'var here'),
+            prepTime = $('<li>').attr('id', 'prepTimeReveal').text('Total time: ' + response.results[i].readyInMinutes + ' minutes'),
             br1 = $('<br>'),
             ingredients = $('<li>').attr('id', 'ingredientReveal').text('Ingredients: ' + "var here"),
             br2 = $('<br>'),
@@ -63,9 +63,12 @@ $(document).ready(function () {
     
           card.append(cardImageDiv, cardContent, cardReveal);
           col.append(card);
-          $('#populate-results').prepend(col);
+          $('#populate-results').append(col);
         }
+        var backtotop = $('<a>').attr({'href':'#top','class':'waves-effect waves-light lime darken-3 btn-small right'}).text('Back to Top'),
+            buttonDiv = $('<div>').append(backtotop);
 
+            $('#results').append(buttonDiv);
       })
    
   }
--- script2.js
+++ script2.js
@@ -1,46 +1,44 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     //spoonacular API call => API key: a1307173fd1545b38ed82223156955bd
 
-    $("button").click(function(event) {
+    $("#cuisineSearch").click(function (event) {
+        event.preventDefault();
 
-        searchQuery = $("#autocomplete-input").val().trim();
+        searchQuery = $("#autocomplete-input").val();
         console.log(searchQuery);
 
-    $.ajax({
-        url: "https://api.spoonacular.com/recipes/complexSearch?query=" + searchQuery + "&recipes&instructionsRequired=true&addRecipeInformation=true&apiKey=a1307173fd1545b38ed82223156955bd",
-        type: "GET",
-        // dataType: "jsonp",
+        if (searchQuery) {
+
+            $.ajax({
+                url: "https://api.spoonacular.com/recipes/complexSearch?query=" + searchQuery + "&recipes&instructionsRequired=true&addRecipeInformation=true&apiKey=a1307173fd1545b38ed82223156955bd",
+                type: "GET",
+            })
+                .then(function (response) {
+                    console.log(response);
+                })
+        }
     })
-    .then(function(response) {
-        console.log(response);
-    })
-})
 
     // Materialize Autocomplete
-    var ac = document.querySelector('.autocomplete');
-    var instance = new M.Autocomplete(ac);
-    // $(document).ready(function() {
     $('.autocomplete').autocomplete({
         data: {
-            "Mexican": null,
             "Margarita": null,
             "Italian": null,
             "Wine": null,
             "Chinese": null
         }
     });
-    // });
 
     // Material Boxed
     var mb = document.querySelectorAll('.materialboxed');
     var instance = new M.Materialbox(mb);
-    // $(document).ready(function() {
+
     $('.mb').mb();
-    // });
+
 
     // $(document).ready(function() {
-    $(".iris").click(function() {
+    $(".iris").click(function () {
         document.body.style.section.backgroundImage = 'url("assets/img/Carousel_2.jpg")';
     });
 });
