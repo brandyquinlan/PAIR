39bbfe2cc917c289f861b31ff7ef1f2c50652735
Delta9k8b <keaton.brewster@gmail.com>
2021-01-20 22:52:27 -0500

    working on parsing out duplicate names

--- assets/js/results.js
+++ assets/js/results.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
       <li id="blurbReveal">
   */
 
-  
+
 
   function getResults(event) {
     event.preventDefault();
@@ -23,24 +23,43 @@ $(document).ready(function () {
 
     var searchVal = $('input').val();
     $.ajax({
-      url: "https://api.spoonacular.com/recipes/complexSearch?query="+searchVal+"&recipes&instructionsRequired=true&addRecipeInformation=true&apiKey=a1307173fd1545b38ed82223156955bd",
+      url: "https://api.spoonacular.com/recipes/complexSearch?query=" + searchVal + "&recipes&instructionsRequired=true&addRecipeInformation=true&apiKey=a1307173fd1545b38ed82223156955bd",
       type: "GET",
     })
       .then(function (response) {
         console.log(response);
-        
+
         for (let i = 0; i < response.results.length; i++) {
+
+
+
+          // Working on getting this to parse out any duplicate ingredients
+          function findAllByKey(obj, keyToFind) {
+            return Object.entries(obj)
+              .reduce((acc, [key, value]) => (key === keyToFind)
+                ? acc.concat(value)
+                : (typeof value === 'object')
+                  ? acc.concat(findAllByKey(value, keyToFind))
+                  : acc
+                , [])
+          }
+          var ingredients = findAllByKey(response.results[i], 'ingredients');
+          let ingredientsNOdupes = [...new Set(ingredients)]
+          console.log(ingredientsNOdupes);
+          // end of this little test function
+
+
           var col = $('<div>').attr('class', 'col s6'),
             card = $('<div>').attr('class', 'card'),
             cardImageDiv = $('<div>').attr('class', 'card-image waves-effect waves-block waves-light'),
             cardImage = $('<img>').attr({ 'src': response.results[i].image, 'alt': 'image of food' }),
-    
+
             cardContent = $('<div>').attr('class', 'card-content'),
             contentSpan = $('<span>').attr('class', 'card-title activator grey-text text-darken-4 truncate').text(response.results[i].title),
             spanI = $('<i>').attr('class', 'material-icons right'),
             contentJlink = $('<p>').attr('id', 'jumplink').text("Jump to Atricle"),
             jLinkHREF = $('<a>').attr('href', '#'),
-    
+
             cardReveal = $('<div>').attr('class', 'card-reveal'),
             revealSpan = $('<span>').attr('class', 'card-title grey-text text-darken-4').text('Quick Look'),
             revealSpanI = $('<i>').attr('class', 'material-icons right').text('close'),
@@ -49,32 +68,32 @@ $(document).ready(function () {
             ul = $('<ul>'),
             prepTime = $('<li>').attr('id', 'prepTimeReveal').text('Total time: ' + response.results[i].readyInMinutes + ' minutes'),
             br1 = $('<br>'),
-            ingredients = $('<li>').attr('id', 'ingredientReveal').text('Ingredients: ' + "var here"),
+            ingredients = $('<li>').attr('id', 'ingredientReveal').text('Ingredients: ' + 'var'),
             br2 = $('<br>'),
             description = $('<li>').attr('id', 'blurbReveal').text('Description: ' + response.results[i].summary),
-    
+
             addFav = $('<a>').attr('class', 'waves-effect waves-light lime darken-3 btn-small right').text('Add Favorite');
-    
+
           cardImageDiv.append(cardImage);
-    
+
           contentSpan.append(spanI);
           contentJlink.append(jLinkHREF);
           cardContent.append(contentSpan, contentJlink);
-    
+
           ul.append(prepTime, br1, ingredients, br2, description);
           revealSpan.append(revealSpanI);
           cardReveal.append(revealSpan, recipeNameH, hr, ul, addFav);
-    
+
           card.append(cardImageDiv, cardContent, cardReveal);
           col.append(card);
           $('#populate-results').append(col);
         }
-        var backtotop = $('<a>').attr({'href':'#top','class':'waves-effect waves-light lime darken-3 btn-small right'}).text('Back to Top'),
-            buttonDiv = $('<div>').append(backtotop);
+        var backtotop = $('<a>').attr({ 'href': '#top', 'class': 'waves-effect waves-light lime darken-3 btn-small right' }).text('Back to Top'),
+          buttonDiv = $('<div>').append(backtotop);
 
-            $('#populate-results').append(buttonDiv);
+        $('#populate-results').append(buttonDiv);
       })
-   
+
   }
 
   $('#search').on('click', getResults)
