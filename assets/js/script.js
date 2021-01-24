6d791ce6d3f9e6d9aa24d59d1ee5bd52ff18682a
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-01-24 08:38:42 -0600

    updated script.js and elimated commented content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
 $(document).ready(function() {
-
     // Image slider
     $('.slider').slider({
         full_width: true,
         'height': 500
     });
 
+    // Initialize modal
+    $('.modal').modal();
+
     // Sidenav for mobile screens
     $('.sidenav').sidenav();
 
     // Materialbox zoom feature for gallery images
     $('.materialboxed').materialbox();
-
-    // Autocomplete for search box
-    // $('.autocomplete').autocomplete({
-    //     data: {
-    //         "Mexican": null,
-    //         "Margarita": null,
-    //         "Italian": null,
-    //         "Wine": null,
-    //         "Chinese": null
-    //     }
-    // });
-
 });
