2886d44622d62e76ede7758511ce3bbd3e8ac665
Delta9k8b <keaton.brewster@gmail.com>
2021-01-23 23:39:36 -0600

    formatting

--- assets/js/results.js
+++ assets/js/results.js
@@ -4,17 +4,6 @@ $(document).ready(function () {
   // Sets up Materialize Navbar + Mobile Toggle
   var instance = M.Sidenav.getInstance('.sidenav');
 
-  /* ID names in HTML for tracking:
-  
-  // Card elements that need targeted API data:
-      <span id="recipeName">
-      <p id="jumpLink">
-      <h6 id="recipeSubName">
-      <li id="prepTimeReveal">
-      <li id="ingredientReveal">
-      <li id="blurbReveal">
-  */
-
 
   // function for creating the html for the results obtained from the search for FOOD
   function getCuisines(event) {
