0243bbf8dbb1e06dcc414d88dc95244cf58f6e0b
Delta9k8b <keaton.brewster@gmail.com>
2021-01-23 23:38:17 -0600

    formatting

--- assets/js/results.js
+++ assets/js/results.js
@@ -30,28 +30,7 @@ $(document).ready(function () {
       .then(function (response) {
         console.log(response);
 
-        for (let i = 0; i < response.results.length; i++) {
-
-          // This seems to be working now, to filter out the ingredients from each recipe pulled
-          function findAllByKey(obj, keyToFind) {
-            return Object.entries(obj).reduce((acc, [key, value]) => (key === keyToFind)
-              ? acc.concat(value)
-              : (typeof value === 'object')
-                ? acc.concat(findAllByKey(value, keyToFind))
-                : acc,
-              [])
-          }
-          var ingredientsList = findAllByKey(response.results[i], 'ingredients');
-          // need to filter for ingredients first, and then use name
-          // was getting an issue where other things in the main object with the key 'name' came up.
-          var ingredients = findAllByKey(ingredientsList, 'name');
-          let ingNoDupes = [...new Set(ingredients)];
-          // formatting the list of ingredients. 
-          let ingredientsArr = [];
-          for (let j = 0; j < ingNoDupes.length; j++) {
-            ingredientsArr.push(ingNoDupes[j]);
-          }
-
+        for (var i = 0; i < response.results.length; i++) {
 
           // Create all the elements with the information pulled
           var col = $('<div>').attr('class', 'col s6 l4'),
@@ -62,8 +41,8 @@ $(document).ready(function () {
             cardContent = $('<div>').attr('class', 'card-content'),
             contentSpan = $('<span>').attr({ 'class': ' activator grey-text text-darken-4 truncate', 'style': 'font-size: 12pt' }).text(response.results[i].title),
             // NEED TO PUT 'SAVE FOR LATER' BUTTON HERE
-            saveBtn = $('<a>').attr({'class': 'waves-effect right btn-flat','href':'#','onclick':'return false;'}),
-            btnI = $('<i>').attr({'class':'material-icons'}).text('bookmark'),
+            saveBtn = $('<a>').attr({ 'data-id': response.results[i].id, 'class': 'waves-effect right btn-flat', 'href': '#', 'onclick': 'return false;' }),
+            btnI = $('<i>').attr({ 'class': 'material-icons' }).text('bookmark'),
             contentJlink = $('<a>').attr({ 'id': 'jumplink', 'href': response.results[i].sourceUrl, 'target': 'blank' }).text("See full recipe"),
 
 
@@ -73,17 +52,19 @@ $(document).ready(function () {
             recipeNameH = $('<h6>').attr({ 'id': 'recipeName' }).text(response.results[i].title),
             hr = $('<hr>'),
             ul = $('<ul>'),
-            prepTime = $('<li>').attr('id', 'prepTimeReveal').text('Total time: ' + response.results[i].readyInMinutes + ' minutes'),
+            prepTime = $('<li>').attr({ 'id': 'prepTimeReveal', 'style': 'font-weight: bold' }).text('Total time: '),
+            prepTimeSpan = $('<span>').attr({ 'style': 'font-weight: lighter' }).text(response.results[i].readyInMinutes + ' minutes'),
             br1 = $('<br>'),
-            ingredients = $('<li>').attr('id', 'ingredientReveal').text('Ingredients: ' + ingredientsArr),
+            ingredients = $('<ul>').attr({ 'id': 'ingredientReveal' + i, 'style': 'font-weight: bold' }).text('Ingredients:'),
             br2 = $('<br>'),
-            description = $('<li>').attr('id', 'blurbReveal').text('Description: '),
-            descriptionSpan = $('<span>'+response.results[i].summary+'</span>');
+            description = $('<li>').attr({ 'id': 'blurbReveal', 'style': 'font-weight: bold' }).text('Description: '),
+            descriptionSpan = $('<span>' + response.results[i].summary + '</span>').attr({ 'style': 'font-weight: lighter' });
 
           // Append all the elements together for presentation
           cardImageDiv.append(cardImage);
           saveBtn.append(btnI);
           cardContent.append(contentSpan, contentJlink, saveBtn);
+          prepTime.append(prepTimeSpan);
           description.append(descriptionSpan);
           ul.append(prepTime, br1, ingredients, br2, description);
           revealSpan.append(revealSpanI);
@@ -92,8 +73,25 @@ $(document).ready(function () {
           col.append(card);
           $('#populate-results').append(col);
 
-          // clear out the ingredients array so they don't overflow to the next item
-          ingredientsArr = [];
+          // now finding, sorting, formatting, and listing all the ingredients for each item
+          function findAllByKey(obj, keyToFind) {
+            return Object.entries(obj).reduce((acc, [key, value]) => (key === keyToFind)
+              ? acc.concat(value)
+              : (typeof value === 'object')
+                ? acc.concat(findAllByKey(value, keyToFind))
+                : acc,
+              [])
+          }
+          var ingredientsList = findAllByKey(response.results[i], 'ingredients');
+          // need to filter for ingredients first, and then use 'name'
+          // was getting an issue where other things in the main object with the key 'name' came up.
+          var ingredients = findAllByKey(ingredientsList, 'name');
+          let ingNoDupes = [...new Set(ingredients)];
+          // formatting the list of ingredients. 
+          for (let j = 0; j < ingNoDupes.length; j++) {
+            var ingredientToList = $('<li>').attr({ 'style': 'font-weight: lighter' }).text(ingNoDupes[j]);
+            $('#ingredientReveal' + i).append(ingredientToList);
+          };
         };
       });
   };
@@ -152,8 +150,8 @@ $(document).ready(function () {
             cardContent = $('<div>').attr('class', 'card-content'),
             contentSpan = $('<span>').attr({ 'class': 'activator grey-text text-darken-4 truncate', 'style': 'font-size: 12pt' }).text(response.drinks[i].strDrink),
             // NEED TO PUT 'SAVE FOR LATER' BUTTON HERE
-            saveBtn = $('<a>').attr({'class': 'waves-effect btn-flat','href':'#','onclick':'return false;'}),
-            btnI = $('<i>').attr({'class':'material-icons'}).text('bookmark'),
+            saveBtn = $('<a>').attr({ 'data-id': response.drinks[i].idDrink, 'class': 'waves-effect btn-flat', 'href': '#', 'onclick': 'return false;' }),
+            btnI = $('<i>').attr({ 'class': 'material-icons' }).text('bookmark'),
 
             cardReveal = $('<div>').attr('class', 'card-reveal'),
             revealSpan = $('<span>').attr('class', 'card-title grey-text text-darken-4').text('Quick Look'),
@@ -186,8 +184,8 @@ $(document).ready(function () {
               var ingredientToList = $('<li>').attr({ 'style': 'font-weight: lighter' }).text(actualIngredients[n]);
               $('#ingredientReveal' + i).append(ingredientToList);
             } else {
-            var ingredientToList = $('<li>').attr({ 'style': 'font-weight: lighter' }).text(actualMeasurements[n] + " " + actualIngredients[n]);
-            $('#ingredientReveal' + i).append(ingredientToList);
+              var ingredientToList = $('<li>').attr({ 'style': 'font-weight: lighter' }).text(actualMeasurements[n] + " " + actualIngredients[n]);
+              $('#ingredientReveal' + i).append(ingredientToList);
             };
           };
           // Accounting for any ingredients that do not have a measurement
