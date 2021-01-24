16322144528203c0eafc46323d6ebeac819833fe
Delta9k8b <keaton.brewster@gmail.com>
2021-01-23 20:22:09 -0600

    adding steves code

--- assets/js/drinksapi.js
+++ assets/js/drinksapi.js
@@ -1,30 +1,63 @@
 $(document).ready(function() {
     
         
-        //This function is calling the drink api.
-        //This URL uses "s=" to search by drink name
-        //Switch to "f=" to search by first letter.
-        //Switch to "i=" to search by ingredient
+  //This function is calling the drink api.
+  //This URL uses "s=" to search by drink name
+  //Switch to "f=" to search by first letter.
+  //Switch to "i=" to search by ingredient
 
-        $.ajax({
-            url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
-            type: "GET",
-    
-        })
-        .then(function(response) {
-            console.log(response);
-    
-            // function findAllByKey(obj, keyToFind) {
-            //     return Object.entries(obj)
-            //       .reduce((acc, [key, value]) => (key === keyToFind)
-            //         ? acc.concat(value)
-            //         : (typeof value === 'object')
-            //         ? acc.concat(findAllByKey(value, keyToFind))
-            //         : acc
-            //       , [])
-            //   }
-            //   // USAGE
-            //   console.log(findAllByKey(response, 'name'));
-        })
-        
-    })
+
+// below is the AJAX call and code that returns:
+// drink name
+// drink instructions
+// drink image (URL)
+// drink ingredients (in an array)
+// incredient measurements (in an array, same order as ingredients array)
+
+
+  $.ajax({
+      url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
+      type: "GET",
+
+  })
+  .then(function(response) {
+      console.log(response);
+      console.log(response.drinks[0].strDrink);
+      console.log(response.drinks[0].strInstructions);
+      console.log(response.drinks[0].strDrinkThumb);
+      // console.log(response);
+      
+      // getting ingredients
+      let filtered_keys = (obj, filter) => {
+        let key, keys = []
+        for (key in obj)
+          if (obj.hasOwnProperty(key) && filter.test(key))
+            keys.push(key)
+        return keys
+      }
+      // example:
+      let possibleIngredients = filtered_keys(response.drinks[0], /strIngredient/);
+      // console.log(possibleIngredients);
+      var actualIngredients = [];
+      for (var i = 0; i < possibleIngredients.length; i++) {
+        if (response.drinks[0][possibleIngredients[i]] !== null) {
+          actualIngredients.push(response.drinks[0][possibleIngredients[i]]);
+        }
+      }
+      console.log(actualIngredients);
+
+      //getting measurements
+        let possibleMeasurements = filtered_keys(response.drinks[0], /strMeasure/);
+      //   console.log(possibleMeasurements);
+        var actualMeasurements = [];
+        for (var i = 0; i < possibleMeasurements.length; i++) {
+          if (response.drinks[0][possibleMeasurements[i]] !== null) {
+            actualMeasurements.push(response.drinks[0][possibleMeasurements[i]]);
+          }
+        }
+        console.log(actualMeasurements);
+      
+
+  })
+  
+})
