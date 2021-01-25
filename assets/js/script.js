e48e684acc9db6959d5e1ddbad08f92c986206ba
Kat Buchanan <katrbuch@gmail.com>
2021-01-24 21:18:33 -0600

    floating action buttons for menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
         height : 333,
     });
 
+    $('.fixed-action-btn').floatingActionButton();
+
     function saveForLater(item, name) {
         localStorage.setItem(name, JSON.stringify(item));
 
